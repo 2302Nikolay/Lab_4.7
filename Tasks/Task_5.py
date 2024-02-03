1f0a881cdcbd16cfd684886404b6ad6ea253f47b
Мальцев Николай <Malzewn@gmail.com>
2024-02-03 12:24:55 +0300

    Изменил пятую

--- Tasks/Task_5.py
+++ Tasks/Task_5.py
@@ -2,32 +2,32 @@
 # -*- coding: utf-8 -*-
 import tkinter as tk
 
-
-class RadioButtonsApp:
-    options = {
-        "Рыбы": "19 февраля - 20 марта",
-        "Козерог": "22 декабря - 19 января",
-        "Скорпион": "23 октября - 21 ноября",
+class ColorPickerApp:
+    colors = {
+        "Красный": "#FF0000",
+        "Зелёный": "#00FF00",
+        "Синий": "#0000FF",
+        "Жёлтый": "#FFFF00",
     }
 
     def __init__(self, root):
         self.root = root
-        self.root.title("Знаки зодиака")
+        self.root.title("Выбор цвета")
 
-        self.selected_option = tk.StringVar()
+        self.selected_color = tk.StringVar()
 
-        self.label_result = tk.Label(root, text="Дата: ")
+        self.label_result = tk.Label(root, text="Выбранный цвет: ")
         self.label_result.pack(pady=10)
 
         self.radio_frame = tk.Frame(root)
         self.radio_frame.pack()
 
-        for key in self.options:
+        for key in self.colors:
             radio_button = tk.Radiobutton(
                 self.radio_frame,
                 text=key,
                 width=10,
-                variable=self.selected_option,
+                variable=self.selected_color,
                 value=key,
                 indicatoron=0,
                 command=self.update_label,
@@ -35,12 +35,12 @@ class RadioButtonsApp:
             radio_button.pack(pady=5, anchor=tk.W)
 
     def update_label(self):
-        selected_option_value = self.selected_option.get()
-        date_range = self.options[selected_option_value]
-        self.label_result.config(text=f"Дата: {date_range}")
+        selected_color_value = self.selected_color.get()
+        hex_code = self.colors[selected_color_value]
+        self.label_result.config(text=f"Выбранный цвет: {selected_color_value}", bg=hex_code)
 
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = RadioButtonsApp(root)
+    app = ColorPickerApp(root)
     root.mainloop()
